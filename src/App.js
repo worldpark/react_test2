af41f084a5da58d8f281496fbae321306d0c2ff5
worldpark <vbwjsdl@naver.com>
2024-04-07 21:47:23 +0900

    리액트 공부2 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Navbar, Container, Nav} from "react-bootstrap";
+import bgImage from "./bg.png";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [productData, setProduct] = useState(
+        [{ 'imgSrc': 'https://codingapple1.github.io/shop/shoes1.jpg'
+            , 'productName': '상품명1', 'productDes': '설명1'}
+        , { 'imgSrc': 'https://codingapple1.github.io/shop/shoes2.jpg'
+            , 'productName': '상품명2', 'productDes': '설명2'}
+        , { 'imgSrc': 'https://codingapple1.github.io/shop/shoes3.jpg'
+            , 'productName': '상품명3', 'productDes': '설명3'}]
+    );
+
+    return (
+        <div className="App">
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">리액트 공부중</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <div className="main-bg" style={{backgroundImage: 'url(' + bgImage + ')'}}>
+            </div>
+
+            <div className="Container">
+                <div className="row">
+                    {
+                        productData.map((content, i) => {
+                            return(
+                                <div className="col-md-4">
+                                    <img src={productData[i].imgSrc} width="80%"/>
+                                    <h4>{productData[i].productName}</h4>
+                                    <p>{productData[i].productDes}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
