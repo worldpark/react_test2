8cf7c40322d143e6b18dcda359a22afdd07fd5d7
worldpark <vbwjsdl@naver.com>
2024-04-20 12:00:16 +0900

    리액트 공부2 240420 공부1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import {Button, Navbar, Container, Nav} from "react-bootstrap";
 import bgImage from "./bg.png";
-import {createContext, useState} from "react";
+import {createContext, lazy, useState, Suspense} from "react";
 import product from "./data";
 import {Routes, Route, useNavigate, Outlet, json} from "react-router-dom"
-import ProductDetailComponent from "./routes/ProductDetailComponent";
+//import ProductDetailComponent from "./routes/ProductDetailComponent";
 import axios from "axios";
-import Cart from "./routes/Cart";
+//import Cart from "./routes/Cart";
 import {useQuery} from "react-query";
+
+//초기 진입시 성능향상 -> 해당 js파일은 해당 페이지 진입시 로드됌 Suspense필요
+const Cart = lazy(() => import('./routes/Cart.js'));
+const ProductDetailComponent = lazy(() => import('./routes/ProductDetailComponent.js'));
 
 export let Context1 = createContext();
 
@@ -40,10 +44,10 @@ function App() {
     let [loadingVisible, setLoadingVisible] = useState(false);
 
     let result = useQuery('user', () =>
-        axios.get('https://codingapple1.github.io/userdata.json')
-            .then((response) => {
-                return response.data;
-            }),
+            axios.get('https://codingapple1.github.io/userdata.json')
+                .then((response) => {
+                    return response.data;
+                }),
         {staleTime: 2000}
     )
 
@@ -68,117 +72,119 @@ function App() {
                 </Container>
             </Navbar>
 
-            <Routes>
-                <Route path="/" element={
-                    <>
-                        <div className="main-bg" style={{backgroundImage: 'url(' + bgImage + ')'}}>
-                        </div>
-                        <button onClick={() => sortBtn()}>sort</button>
-                        <div className="Container">
-                            <div className="row">
-                                {
-                                    productData.map((content, i) => {
-                                        return (
-                                            <div className="col-md-4" key={i}
-                                                 onClick={() => navigate('/detail/' + productData[i].id)}>
-                                                <HomeComponent
-                                                    products={productData[i]}
-                                                />
-                                            </div>
-                                        )
-                                    })
-                                }
+            <Suspense fallback={<div>로딩중</div>}>
+                <Routes>
+                    <Route path="/" element={
+                        <>
+                            <div className="main-bg" style={{backgroundImage: 'url(' + bgImage + ')'}}>
                             </div>
-                        </div>
-                        {
-                            buttonVisible ?
-                                loadingVisible ?
-                                    <div>
-                                        로딩중...
-                                    </div> :
-                                    <button onClick={() => {
-                                        setLoadingVisible(true);
+                            <button onClick={() => sortBtn()}>sort</button>
+                            <div className="Container">
+                                <div className="row">
+                                    {
+                                        productData.map((content, i) => {
+                                            return (
+                                                <div className="col-md-4" key={i}
+                                                     onClick={() => navigate('/detail/' + productData[i].id)}>
+                                                    <HomeComponent
+                                                        products={productData[i]}
+                                                    />
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                            {
+                                buttonVisible ?
+                                    loadingVisible ?
+                                        <div>
+                                            로딩중...
+                                        </div> :
+                                        <button onClick={() => {
+                                            setLoadingVisible(true);
 
-                                        if (buttonClickCount == 0) {
-                                            axios.get('https://codingapple1.github.io/shop/data2.json')
-                                                .then((response) => {
+                                            if (buttonClickCount == 0) {
+                                                axios.get('https://codingapple1.github.io/shop/data2.json')
+                                                    .then((response) => {
 
-                                                    let resData = response.data;
-                                                    for (let i = 0; i < resData.length; i++) {
-                                                        resData[i].imgSrc = 'https://codingapple1.github.io/shop/shoes1.jpg';
-                                                        resData[i].productName = resData[i].title;
-                                                        resData[i].productDes = resData[i].content;
-                                                    }
+                                                        let resData = response.data;
+                                                        for (let i = 0; i < resData.length; i++) {
+                                                            resData[i].imgSrc = 'https://codingapple1.github.io/shop/shoes1.jpg';
+                                                            resData[i].productName = resData[i].title;
+                                                            resData[i].productDes = resData[i].content;
+                                                        }
 
-                                                    let copy = [
-                                                        ...productData,
-                                                        ...resData];
+                                                        let copy = [
+                                                            ...productData,
+                                                            ...resData];
 
-                                                    setProduct(copy);
-                                                    setLoadingVisible(false);
-                                                })
-                                                .catch((error) => {
-                                                    console.log(error);
-                                                    setLoadingVisible(false);
-                                                })
-                                        } else if (buttonClickCount == 1) {
-                                            axios.get('https://codingapple1.github.io/shop/data3.json')
-                                                .then((response) => {
+                                                        setProduct(copy);
+                                                        setLoadingVisible(false);
+                                                    })
+                                                    .catch((error) => {
+                                                        console.log(error);
+                                                        setLoadingVisible(false);
+                                                    })
+                                            } else if (buttonClickCount == 1) {
+                                                axios.get('https://codingapple1.github.io/shop/data3.json')
+                                                    .then((response) => {
 
-                                                    let resData = response.data;
-                                                    for (let i = 0; i < resData.length; i++) {
-                                                        resData[i].imgSrc = 'https://codingapple1.github.io/shop/shoes1.jpg';
-                                                        resData[i].productName = resData[i].title;
-                                                        resData[i].productDes = resData[i].content;
-                                                    }
+                                                        let resData = response.data;
+                                                        for (let i = 0; i < resData.length; i++) {
+                                                            resData[i].imgSrc = 'https://codingapple1.github.io/shop/shoes1.jpg';
+                                                            resData[i].productName = resData[i].title;
+                                                            resData[i].productDes = resData[i].content;
+                                                        }
 
-                                                    let copy = [
-                                                        ...productData,
-                                                        ...resData];
+                                                        let copy = [
+                                                            ...productData,
+                                                            ...resData];
 
-                                                    setProduct(copy);
-                                                    setLoadingVisible(false);
-                                                })
-                                                .catch((error) => {
-                                                    console.log(error);
-                                                    setLoadingVisible(false);
-                                                })
-                                            setButtonVisible(false);
-                                        }
+                                                        setProduct(copy);
+                                                        setLoadingVisible(false);
+                                                    })
+                                                    .catch((error) => {
+                                                        console.log(error);
+                                                        setLoadingVisible(false);
+                                                    })
+                                                setButtonVisible(false);
+                                            }
 
-                                        setButtonClickCount(buttonClickCount + 1);
-                                    }}>
-                                        더보기
-                                    </button> : null
-                        }
+                                            setButtonClickCount(buttonClickCount + 1);
+                                        }}>
+                                            더보기
+                                        </button> : null
+                            }
 
-                    </>
-                }/>
-                <Route path="/detail/:id" element={
-                    <Context1.Provider value={{stock}}>
-                        <ProductDetailComponent product={productData}/>
-                    </Context1.Provider>
-                }/>
-                <Route path="/about" element={<About/>}>
-                    <Route path="member" element={<div>멤버임</div>}/>
-                    <Route path="location" element={<div>위치임</div>}/>
-                </Route>
+                        </>
+                    }/>
+                    <Route path="/detail/:id" element={
+                        <Context1.Provider value={{stock}}>
+                            <ProductDetailComponent product={productData}/>
+                        </Context1.Provider>
+                    }/>
+                    <Route path="/about" element={<About/>}>
+                        <Route path="member" element={<div>멤버임</div>}/>
+                        <Route path="location" element={<div>위치임</div>}/>
+                    </Route>
 
-                <Route path="/event" element={<Event/>}>
-                    <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
-                    <Route path="two" element={<div>생일기념 쿠폰받기</div>}></Route>
-                </Route>
+                    <Route path="/event" element={<Event/>}>
+                        <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+                        <Route path="two" element={<div>생일기념 쿠폰받기</div>}></Route>
+                    </Route>
 
-                <Route path="/cart" element={
-                    <Cart/>
-                }/>
+                    <Route path="/cart" element={
+                        <Cart/>
+                    }/>
 
-                <Route path="*" element={
-                    <>
-                        <div>404 page</div>
-                    </>
-                }/>
-            </Routes>
+                    <Route path="*" element={
+                        <>
+                            <div>404 page</div>
+                        </>
+                    }/>
+                </Routes>
+            </Suspense>
         </div>
     );
 }
--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -2,15 +2,39 @@ import {Table} from "react-bootstrap";
 import {useDispatch, useSelector} from "react-redux";
 import {setUserName, setUserAge} from "./../store/userSlice";
 import {setCount, setStock} from "../store";
+import {memo, useMemo, useState} from "react";
+
+const test = () => {
+    return 'aa';
+}
+
+//해당 컴포넌트 props가 변경시에만 재랜더링
+const Child = memo( () => {
+    console.log('rendering');
+
+    return <div>자식임</div>
+})
 
 const Cart = () => {
+
+    //처음 렌더링시에만 작동
+    let result = useMemo(() => {
+        return test();
+    }, )
 
     let user = useSelector((state) => state.user);
     let cart = useSelector((state) => state.cart);
     let dispatch = useDispatch();
 
+    let [count, setCount] = useState(0);
+
     return (
         <div>
+            <Child count={count}/>
+            <button onClick={() => {
+                setCount(count+1)
+            }}></button>
+
             {user.name}, {user.age}의 장바구니
             <button onClick={() => {
                 dispatch(setUserAge(1))
